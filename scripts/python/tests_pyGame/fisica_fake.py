5d8829c0f1ae8a45b5e47ad6a894f938c404171c
D. Gabriel <mendesdouglasgabriel30@gmail.com>
2026-01-20 23:48:19 -0300

    eu pensei que isso não funcionaria... mas foi mais facil do que eu pensei

--- scripts/python/tests_pyGame/fisica_fake.py
+++ scripts/python/tests_pyGame/fisica_fake.py
@@ -13,6 +13,22 @@ pygame.display.set_caption("Fisica Fake")
 clock = pygame.time.Clock()
 
 
+x = 400
+y = 300
+
+raio = 10
+
+
+
+
+vx = 3
+vy = 2
+
+
+
+#Teste
+gravidade = 0.4
+
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
@@ -20,5 +36,22 @@ while True:
             sys.exit()
         
     tela.fill((20, 20, 20))
+
+    x += vx
+
+    vy+= gravidade
+    y += vy
+    if x - raio <= 0 or x + raio >= LARGURA:
+        vx = -vx
+    if y - raio <= 0 or y + raio >= LARGURA:
+        vy = -vy
+
+    if y + raio >= ALTURA:
+        y = ALTURA -raio
+        vy = -vy * 0.8
+
+
+
+    pygame.draw.circle(tela, (200,200,200), (int(x), int(y)), raio)
     pygame.display.flip()
     clock.tick(60)
